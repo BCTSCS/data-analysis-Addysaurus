9a70ca6592cb6b8b40d377893379515f8700fae7
Addysaurus <addisonalmagor@gmail.com>
2026-01-09 14:10:51 -0500

    regex

---
+++ Regex.java
@@ -0,0 +1,17 @@
+import java.util.regex.*;
+import java.util.ArrayList;
+
+public class Regex {
+    public static void main(String[] args) {
+        String re = "#\\w\\s";
+        // String text = "BAABBAAB";
+        // boolean result = text.matches(re);
+        // System.out.println(result);
+
+        //READING POSTS
+        ArrayList<String> posts = FileOperator.getStringList("posts.txt")
+        for (String post : posts) {
+            String result post.
+        }
+    }
+}
